e1936ce938eed8ae113f9d084d4b7f76f923312b
Louis <menacho.louis@gmail.com>
2022-05-18 01:49:51 -0400

    Separate cells in RoomInfoViewController by sections

--- QueUp/Controller/RoomInfoViewController.swift
+++ QueUp/Controller/RoomInfoViewController.swift
@@ -54,27 +54,27 @@ class RoomInfoViewController: UIViewController {
 extension RoomInfoViewController: UITableViewDataSource {
     
     func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
-    }
-    
-    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 3
     }
     
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1
+    }
+    
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.row == 0 {
+        if indexPath.section == 0 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "SpotifyLinkTableViewCell", for: indexPath) as! SpotifyLinkTableViewCell
             cell.delegate = self
             cell.update(with: roomVM.room)
             return cell
         }
-        if indexPath.row == 1 {
+        if indexPath.section == 1 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "RoomInfoTableViewCell", for: indexPath)
             cell.textLabel?.text = "Clear Playlist"
             cell.textLabel?.textColor = .label
             return cell
         }
-        if indexPath.row == 2 {
+        if indexPath.section == 2 {
             let cell = tableView.dequeueReusableCell(withIdentifier: "RoomInfoTableViewCell", for: indexPath)
             cell.textLabel?.text = "End Room Session"
             cell.textLabel?.textColor = .red
@@ -86,8 +86,25 @@ extension RoomInfoViewController: UITableViewDataSource {
 
 extension RoomInfoViewController: UITableViewDelegate {
     
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 44
+    }
+    
+    func tableView(_ tableView: UITableView, titleForFooterInSection section: Int) -> String? {
+        if section == 0 {
+            return "Create a playlist on Spotify named \"QueUp Room \(roomVM.room.id)\"."
+        }
+        if section == 1 {
+            return "Remove all songs from playlist. If Spotify is linked, this will also remove all songs from the \"QueUp Room \(roomVM.room.id)\" playlist."
+        }
+        if section == 2 {
+            return ""
+        }
+        return nil
+    }
+    
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        if indexPath.row == 1 {
+        if indexPath.section == 1 {
             showActionSheet(title: "Are you sure you want to clear playlist?", action: .init(title: "Clear Playlist", style: .destructive) {  action in
                 Task {
                     let result = await self.roomVM.clearPlaylist()
@@ -100,7 +117,7 @@ extension RoomInfoViewController: UITableViewDelegate {
                 }
             })
         }
-        if indexPath.row == 2 {
+        if indexPath.section == 2 {
             showActionSheet(title: "Are you sure you want to end room session?", action: .init(title: "End Room Session", style: .destructive) {  action in
                 Task {
                     let result = await self.roomVM.closeRoom()
@@ -114,10 +131,6 @@ extension RoomInfoViewController: UITableViewDelegate {
             })
         }
         tableView.deselectRow(at: indexPath, animated: true)
-    }
-    
-    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
-        return 44
     }
 }
 
