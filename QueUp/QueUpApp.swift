306196712dcbde2c08a174c0082bfed90723bf86
louis <louis.menacho@icloud.com>
2024-10-19 19:55:49 -0400

    Initial Commit

---
+++ QueUp/QueUpApp.swift
@@ -0,0 +1,17 @@
+//
+//  QueUpApp.swift
+//  QueUp
+//
+//  Created by Louis Menacho on 10/19/24.
+//
+
+import SwiftUI
+
+@main
+struct QueUpApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
